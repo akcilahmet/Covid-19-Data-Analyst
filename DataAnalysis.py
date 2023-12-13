c85b43223056e443fd96628a94eb7cb6ac419317
Ahmet Akçıl <83040124+akcilahmet@users.noreply.github.com>
2023-12-13 22:20:27 +0300

    DataAnalysis.py update

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,5 +1,6 @@
 from DataBase import MongoDBManager
 import pandas as pd
+import matplotlib.pyplot as plt
 
 mongoDb_instance=MongoDBManager()
 data=mongoDb_instance.get_data()
@@ -15,3 +16,25 @@ dataFrame=pd.DataFrame(data)
 # print(dataFrame.isnull().sum()) # sütuna ait none değer sayisini döner
 
 # print(dataFrame.duplicated().sum()) # yinelenen kayit sayisini verir
+
+#
+# dataFrame.rename(columns={'Country/Region':'Test'},inplace=True) #Belirtilen columns isim degistirmesini saglar
+# print(dataFrame.columns)
+
+#bölgelere göre vaka sayilari
+confirmed_by_country=dataFrame.groupby('Country/Region')['Confirmed'].sum().sort_values(ascending=False)
+print(confirmed_by_country)
+
+
+#yüzde hesaplama
+percentage=confirmed_by_country / confirmed_by_country.sum()*100
+print(percentage)
+
+#top10
+top_10_percentage=percentage.head(10)
+#daire grafige cizme
+plt.figure(figsize=(10,10))
+
+plt.pie(top_10_percentage,labels=top_10_percentage.index,autopct='%1.1f%%',startangle=140,shadow=True)
+plt.title(f'Percentage Distribution of Confirmed Cases by Country')
+plt.show()
